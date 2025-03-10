6b617a01e1328f907e9958a1a8eb37c06701c981
kahput <kahputs@gmail.com>
2025-03-10 15:24:01 +0100

    feat: initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-800 flex justify-center items-center">
+      <h1 className="text-6xl text-gray-100 font-extrabold">Time Tracking App</h1>
+    </div>
+  )
+}
+
+export default App
